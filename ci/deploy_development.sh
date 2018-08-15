1ac061724d3fffbbb2432a349881fd9bec5e44fc
horike37 <horike37@gmail.com>
2018-08-16 05:26:19 +0900

    リージョンとステージの情報を環境変数にする

--- ci/deploy_development.sh
+++ ci/deploy_development.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Deploying to stage development"
-./node_modules/.bin/serverless deploy --stage development --region ap-northeast-1 --verbose
+./node_modules/.bin/serverless deploy --stage development --region getenv('AWS_DEFAULT_REGION') --verbose
--- ci/deploy_production.sh
+++ ci/deploy_production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Deploying to stage production"
-./node_modules/.bin/serverless deploy --stage production --region ap-northeast-1 --verbose
+./node_modules/.bin/serverless deploy --stage production --region getenv('AWS_DEFAULT_REGION') --verbose
