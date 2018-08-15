3d3acd010882d12915d837048be5cd766a3288c3
horike37 <horike37@gmail.com>
2018-08-15 11:46:55 +0900

    fix region to ap-northeast-1

--- ci/deploy_development.sh
+++ ci/deploy_development.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Deploying to stage development"
-./node_modules/.bin/serverless deploy --stage development --verbose
+./node_modules/.bin/serverless deploy --stage development --region ap-northeast-1 --verbose
--- ci/deploy_production.sh
+++ ci/deploy_production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Deploying to stage production"
-./node_modules/.bin/serverless deploy --stage production --verbose
+./node_modules/.bin/serverless deploy --stage production --region ap-northeast-1 --verbose
