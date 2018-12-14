46eded52ed224c511557c6b4aaa22734bf99aeec
horike37 <horike37@gmail.com>
2018-12-14 15:19:24 +0900

    update

--- sh/deploy_db_production.sh
+++ sh/deploy_db_production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Deploying DB to stage production"
 cd db
-./node_modules/.bin/serverless deploy --stage production --region ${AWS_DEFAULT_REGION} --alias ${API_VERSION} --verbose
+./../node_modules/.bin/serverless deploy --stage production --region ${AWS_DEFAULT_REGION} --alias ${API_VERSION} --verbose
 cd ../
